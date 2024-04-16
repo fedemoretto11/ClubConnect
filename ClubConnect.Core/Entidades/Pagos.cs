ef51176bf5b9f44d47f83cea700ff75ce6a65e53
fedemoretto11 <fedemoretto94@gmail.com>
2024-04-16 20:39:31 -0300

    Commit inicial

---
+++ ClubConnect.Core/Entidades/Pagos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubConnect.Core
+{
+	internal class Pagos
+	{
+		private int dni { get; set; }
+		private string nombre { get; set; }
+		private string apellido { get; set; }
+		private DateTime fechaDeNacimiento { get; set; }
+		private string direccion {  get; set; }
+		private string email {  get; set; }
+		private string telefono {  get; set; }
+		private DateTime fechaDeRegistro { get; set; }
+		private Boolean estaActivo { get; set; }
+	}
+}
